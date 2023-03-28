3433480b967d9af7cab277f0f4d799fa485a0235
KingsSack <58608657+KingsSack@users.noreply.github.com>
2023-03-28 07:35:34 -0700

    Initial commit

---
+++ entityculling/src/main/java/com/lasteditguild/Main.java
@@ -0,0 +1,9 @@
+package com.lasteditguild;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    // create a minecraft entity culling function
+}
